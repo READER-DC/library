3bcae6fcfc291ce7430b8581253490ac1ecd6c9d
Anatolii Sehal <sab198005@gmail.com>
2024-08-03 19:50:25 +0300

    add fly way and postgre db

--- src/main/java/com/example/services/BookService.java
+++ src/main/java/com/example/services/BookService.java
@@ -1,33 +1,63 @@
 package com.example.services;
 
-import java.util.Collections;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.model.Book;
+import com.example.repository.BookRepository;
 
 @Service
 public class BookService {
 	
+	@Autowired
+	private BookRepository bookRepository;
+	
+	
 	public Book getBook(Long id) {
-		return new Book();
+		Optional<Book> optional = bookRepository.findById(id);
+		if (optional.isPresent()) {
+			return optional.get();
+		}
+		throw new RuntimeException("Book not found");
 	}
 	
 	public List<Book> getAllBook(){
-		return Collections.emptyList();
+		List<Book> dooks = new ArrayList<Book>();
+		Iterator<Book> iterator = bookRepository.findAll().iterator();
+		while (iterator.hasNext()){
+			Book itemBook = iterator.next();
+			dooks.add(itemBook);
+		}
+		return dooks;
 	}
 	
 	public Book crerateBook(Book book) {
-		return book;
+		Book savedBook = bookRepository.save(book);
+		return savedBook;
 	}
 	
 	public Book editBook(Long id, Book book) {
-		return book;
+		Book originBook = getBook(id);
+		
+		originBook.setAuthor(book.getAuthor());
+		originBook.setDescription(book.getAuthor());
+		originBook.setIsbn(book.getIsbn());
+		originBook.setName(book.getName());
+		originBook.setPublisher(book.getPublisher());
+		originBook.setYear(book.getYear());
+		
+		Book updatedBook =  bookRepository.save(originBook);
+		
+		return updatedBook;
 	}
 	
 	public void deleteBook(Long id) {
-		
+		bookRepository.deleteById(id);
 	}
 
 }
---
+++ src/main/resources/db/migration/V01__book_table.sql
@@ -0,0 +1,9 @@
+create table book(
+	id bigserial not null,
+	name varchar(255) not null,
+	author varchar(255) not null,
+	description text,
+	publisher varchar(255),
+	isbn varchar(25),
+	year int
+)

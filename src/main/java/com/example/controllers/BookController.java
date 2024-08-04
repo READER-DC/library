541703b1e0267a412f46f115dca71713e6154c79
Anatolii Sehal <sab198005@gmail.com>
2024-08-04 14:08:20 +0300

    create tests and git action

--- src/main/java/com/example/controllers/BookController.java
+++ src/main/java/com/example/controllers/BookController.java
@@ -1,6 +1,7 @@
 package com.example.controllers;
 
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.model.Book;
@@ -38,6 +40,7 @@ public class BookController {
 	}
 
 	@PostMapping("/book")
+	@ResponseStatus(code = HttpStatus.CREATED)
 	public Book createBook(@RequestBody Book book) {
 		return bookService.crerateBook(book);
 	}
